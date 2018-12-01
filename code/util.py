3595f2d01eebed046db6ad860a3dbf2745f18411
ZhengErWei <31546250+ZhengErWei@users.noreply.github.com>
2018-12-01 12:17:35 -0600

    Update util.py

--- code/util.py
+++ code/util.py
@@ -1,29 +1,13 @@
 import praw
 import math
 
-REDDIT_1 = praw.Reddit(client_id = 'Ip1jf64PVY6_QQ', 
-						client_secret = 'qIBcz5jWTmcqs5TpPM9SDpmKYlo', 
-						username = 'ErWeiZheng', 
-						password= 'TesanZheng1006', 
-						user_agent='praw_trial')
+REDDIT_1 
 
-REDDIT_2 = praw.Reddit(client_id = 'FGVEIcwf1MNiIw', 
-						client_secret = '8cTiak9cFTx0s0W4EOMOg_o7Bag', 
-						username = 'ErWeiZheng', 
-						password= 'TesanZheng1006', 
-						user_agent='praw_trial_2')
+REDDIT_2 
 
-REDDIT_3 = praw.Reddit(client_id = 'BclBM7ciAkAEqA', 
-						client_secret = 'gDXfV5-Co0To-GRUdtdIJqB0qUY', 
-						username = 'ErWeiZheng', 
-						password= 'TesanZheng1006', 
-						user_agent='praw_trial_3')
+REDDIT_3 
 
-REDDIT_4 = praw.Reddit(client_id = 'SzuNTymQSs0jQg', 
-						client_secret = 'Q1Y0IF9O0nT62LS2vj5YuNgAdBU', 
-						username = 'ErWeiZheng', 
-						password= 'TesanZheng1006', 
-						user_agent='praw_trial_4')
+REDDIT_4 
 
 REDDITS = [REDDIT_1, REDDIT_2, REDDIT_3, REDDIT_4]
 
